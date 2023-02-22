56df1869f821feebb139e30ca251b24eea6bf0e1
JustKooba <109106079+JustKooba@users.noreply.github.com>
2023-02-22 19:35:23 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header-main />
+  </div>
+</template>
+
+<script>
+import HeaderMain from "./components/Header/Header.vue";
+export default {
+  components: {
+    HeaderMain,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "./components/Header/style.css";
+createApp(App).mount("#app");
