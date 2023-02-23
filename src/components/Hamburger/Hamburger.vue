84d60d872bcdfcb1c74710350b585bb7b2acb882
JustKooba <109106079+JustKooba@users.noreply.github.com>
2023-02-23 19:04:47 +0100

    Add files via upload

---
+++ src/components/Hamburger/Hamburger.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="menu">
+    <img src="../../assets/menu.svg" class="menu" @click="handleMenu" />
+    <div class="content-menu" :style="{ display: menuDisplay }">
+      <p>
+        TODO Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Accusantium earum tempora
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hamburger",
+  data() {
+    return {
+      menuDisplay: "none",
+    };
+  },
+  methods: {
+    handleMenu() {
+      if (this.menuDisplay == "none") {
+        this.menuDisplay = "block";
+      } else if (this.menuDisplay == "block") {
+        this.menuDisplay = "none";
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <header>
+      <div class="user-greet">
+        <img src="../../assets/profile-placeholder.svg" class="profile-pic" />
+        <span>
+          <p class="hello">Hello</p>
+          <p class="user">Username</p>
+        </span>
+      </div>
+      <hamburger />
+    </header>
+  </main>
+</template>
+
+<script>
+import Hamburger from "../Hamburger/Hamburger.vue";
+
+export default {
+  name: "HeaderMain",
+  components: {
+    Hamburger,
+  },
+};
+</script>
+
+<style></style>
