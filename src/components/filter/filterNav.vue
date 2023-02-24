d671babd44d202d787c916981267f11ed1ff6954
JustKooba <109106079+JustKooba@users.noreply.github.com>
2023-02-24 19:04:45 +0100

    Add files via upload

---
+++ src/components/filter/filterNav.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="input-container">
+      <input type="text" />
+      <button><img src="../../assets/submit.svg" alt="" /></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "filterNav",
+};
+</script>
+
+<style></style>
