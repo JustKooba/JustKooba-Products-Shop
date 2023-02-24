516c93b4bb1ccbea061d48c16f98ddd5555a73cf
JustKooba <109106079+JustKooba@users.noreply.github.com>
2023-02-24 19:01:27 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from "./App.vue";
 
 import "./components/Header/style.css";
 import "./components/Hamburger/style.css";
+import "./components/filter/style.css";
 
 createApp(App).mount("#app");
