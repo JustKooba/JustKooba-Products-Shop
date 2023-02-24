1d2e88e239406ff76a1b43daf7965fdbd73648f3
JustKooba <109106079+JustKooba@users.noreply.github.com>
2023-02-24 19:05:08 +0100

    Imported new component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <header-main />
+    <filter-nav />
   </div>
 </template>
 
 <script>
 import HeaderMain from "./components/Header/Header.vue";
+import filterNav from "./components/filter/filterNav.vue";
 export default {
   components: {
     HeaderMain,
+    filterNav,
   },
 };
 </script>
