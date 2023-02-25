b5cf63395c6bd01b23bb1c7fb074735a5ed42834
JustKooba <109106079+JustKooba@users.noreply.github.com>
2023-02-25 15:22:04 +0100

    Importing products

---
+++ src/components/products/products.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="product-card" v-for="product in products" :key="product.id">
+      <h1>Name:{{ product.title }}</h1>
+      <p>Desc:{{ product.description }}</p>
+      <span>Price:{{ product.price }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+import axios from "axios";
+
+export default {
+  name: "productsVue",
+  setup() {
+    const products = reactive({
+      data: [],
+    });
+
+    async function fetchData() {
+      try {
+        const res = await axios.get("https://dummyjson.com/products");
+        products.data = res.data;
+        console.log(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+
+    return {
+      products,
+    };
+  },
+};
+</script>
+
+<style></style>
