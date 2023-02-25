861bd11a63d6ca64b9bf579b8aaf2730b8aa5b69
JustKooba <109106079+JustKooba@users.noreply.github.com>
2023-02-25 15:22:27 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div>
     <header-main />
     <filter-nav />
+    <products-vue />
   </div>
 </template>
 
 <script>
 import HeaderMain from "./components/Header/Header.vue";
 import filterNav from "./components/filter/filterNav.vue";
+import productsVue from "./components/products/products.vue";
 export default {
   components: {
     HeaderMain,
     filterNav,
+    productsVue,
   },
 };
 </script>
