bf0c5c963344bddceda651d1c1c44af5b0e2cc1c
JustKooba <109106079+JustKooba@users.noreply.github.com>
2023-03-01 12:40:38 +0100

    added a button carousel

--- src/components/filter/filterNav.vue
+++ src/components/filter/filterNav.vue
@@ -5,12 +5,46 @@
         <input type="text" placeholder="Search Products" />
       </form>
     </div>
+    <div class="carousel" v-dragscroll>
+      <div class="item">
+        <button>
+          <img src="../../assets/phone.svg" alt=" Laptop icon" /> Phones
+        </button>
+      </div>
+      <div class="item">
+        <button>
+          <img src="../../assets/laptop.svg" alt="laptop icon" /> Laptops
+        </button>
+      </div>
+      <div class="item">
+        <button>
+          <img
+            src="../../assets/perfume.svg"
+            alt="perfume icon"
+            class="perfume"
+          />
+          Perfumes
+        </button>
+      </div>
+      <div class="item">
+        <button>
+          <img src="../../assets/oil.svg" alt="droplet icon" /> Oils
+        </button>
+      </div>
+      <div class="item"><button>Care</button></div>
+      <div class="item"><button>Home</button></div>
+      <div class="item"><button>Serums</button></div>
+    </div>
   </div>
 </template>
 
 <script>
+import { dragscroll } from "vue-dragscroll";
 export default {
   name: "filterNav",
+  directives: {
+    dragscroll: dragscroll,
+  },
 };
 </script>
 
