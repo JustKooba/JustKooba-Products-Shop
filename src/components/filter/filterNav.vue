b8b2f8facd452a9c4169f8c666ce85cc8086ea1c
JustKooba <109106079+JustKooba@users.noreply.github.com>
2023-03-04 17:14:09 +0100

    Made products.vue a child component

--- src/components/filter/filterNav.vue
+++ src/components/filter/filterNav.vue
@@ -7,17 +7,17 @@
     </div>
     <div class="carousel" v-dragscroll>
       <div class="item">
-        <button ref="button" @click="sortProducts" v-on:click="handleClick">
+        <button ref="button" @click="sortProducts('smartphones')">
           <img src="../../assets/phone.svg" alt="Laptop icon" /> smartphones
         </button>
       </div>
       <div class="item">
-        <button ref="button" @click="sortProducts" v-on:click="handleClick">
+        <button ref="button" @click="sortProducts('laptops')">
           <img src="../../assets/laptop.svg" alt="laptop icon" /> Laptops
         </button>
       </div>
       <div class="item">
-        <button ref="button" @click="sortProducts" v-on:click="handleClick">
+        <button ref="button" @click="sortProducts('fragrances')">
           <img
             src="../../assets/perfume.svg"
             alt="perfume icon"
@@ -27,26 +27,32 @@
         </button>
       </div>
       <div class="item">
-        <button ref="button" @click="sortProducts" v-on:click="handleClick">
+        <button ref="button" @click="sortProducts('skincare')">
           <img src="../../assets/oil.svg" alt="droplet icon" /> skincare
         </button>
       </div>
 
       <div class="item">
-        <button ref="button" v-on:click="handleClick" @click="sortProducts">
+        <button ref="button" @click="sortProducts('home-decoration')">
           <img src="../../assets/home.svg" alt="home icon" /> home-decoration
         </button>
       </div>
     </div>
   </div>
+  <products-vue @sortProducts="handleSort" />
 </template>
 
 <script>
 import { dragscroll } from "vue-dragscroll";
+import productsVue from "../products/products.vue";
+
 export default {
   name: "filterNav",
   directives: {
     dragscroll: dragscroll,
+  },
+  components: {
+    productsVue,
   },
   data() {
     return {
@@ -54,13 +60,10 @@ export default {
     };
   },
   methods: {
-    sortProducts() {
-      this.$emit("sortProducts", this.category);
-    },
-    handleClick() {
-      
-      this.category = event.target.innerText;
+    sortProducts(category) {
+      this.category = category;
       console.log(this.category);
+      this.$emit("sortProducts", this.category);
     },
   },
 };
